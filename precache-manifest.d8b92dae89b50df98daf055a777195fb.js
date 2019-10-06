6957e926b3675f0f10f1447fe5d0b47930a789e8
Sibasish Mohanty <smsibasish@gmail.com>
2019-10-06 19:57:06 +0530

    Updates

---
+++ precache-manifest.d8b92dae89b50df98daf055a777195fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf97225cde5091b4d8f83e794e48beb",
+    "url": "/Sibasish19/pokebook/index.html"
+  },
+  {
+    "revision": "aaf4f0c408070039195f",
+    "url": "/Sibasish19/pokebook/static/css/main.9ffe4d76.chunk.css"
+  },
+  {
+    "revision": "bd6df359bdb70a4332ab",
+    "url": "/Sibasish19/pokebook/static/js/2.81cd896c.chunk.js"
+  },
+  {
+    "revision": "aaf4f0c408070039195f",
+    "url": "/Sibasish19/pokebook/static/js/main.0d372ae5.chunk.js"
+  },
+  {
+    "revision": "83d1f1e25da840dc071e",
+    "url": "/Sibasish19/pokebook/static/js/runtime-main.1a6faf98.js"
+  },
+  {
+    "revision": "37ba67e42fe0596d1263e4e85f2387ec",
+    "url": "/Sibasish19/pokebook/static/media/hero-image.37ba67e4.jpg"
+  }
+]);
